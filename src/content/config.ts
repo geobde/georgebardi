40290cd42fff5fb8ef16557799f21e1f433c9f4d
geobde <geobde@gmail.com>
2023-09-09 17:07:00 +0300

    finalize  fixes

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,11 +4,7 @@ export const collections = {
   article: defineCollection({
     schema: z.object({
       title: z.string(),
-      description: z.string(),
-      publishDate: z.coerce.date(),
-      tags: z.array(z.string()),
-      img: z.string(),
-      img_alt: z.string().optional(),
+      link: z.string(),
     }),
   }),
 };
