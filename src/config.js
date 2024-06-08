fded887dfaefddd8526d6fbef7c111c05e09f9ff
geobde <geobde@gmail.com>
2024-06-09 01:38:12 +0300

    update text

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ const config = {
     ],
     hero: {
      title: "Hey, I'm George Bardi 👋",
-     tagline: "Engineer specializing in Javascript & Web Performance. I currently work as Senior Frontend Engineer @ Intelligencia AI",
+     tagline: "Crafting interfaces. I currently work as Senior Frontend Engineer @ Intelligencia AI",
      image:"/assets/portrait.jpeg",
      section:" I try to keep things simple. You'll find writing about technologies I'm interested in at the time."
     },
